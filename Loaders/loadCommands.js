b854656aa5aff19a0973e709b7553366a6930cd2
WWiRRu <92327076+WWiRRu@users.noreply.github.com>
2022-09-13 15:01:15 +0200

    Changement de syntaxe

--- Loaders/loadCommands.js
+++ Loaders/loadCommands.js
@@ -6,7 +6,7 @@ module.exports = async (bot) => {
         if (!command.name || typeof command.name !== 'string') throw new TypeError(`La commande ${file.slice(0, file.length - 3)}} n'a pas de nom !`);
         bot.commands.set(command.name, command);
         console.log("--------------------------------------");
-        console.log(`La command ${file} est chargé avec succès.`);
+        console.log(`Commande: ${file} est chargé avec succès.`);
         console.log("--------------------------------------");
     }
 }
